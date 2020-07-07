e11f51fefa14dcc41ce5d07e8af67c62780e43b4
Roderic Page <rdmpage@gmail.com>
2020-07-07 11:11:24 +0100

    Extract references

--- extract-references.php
+++ extract-references.php
@@ -7,16 +7,68 @@ use Sunra\PhpSimple\HtmlDomParser;
 
 
 $basedir = dirname(__FILE__) . '/articles';
-$sourcedir = dirname(__FILE__) . '/contents';
+$sourcedir = dirname(__FILE__) . '/articles';
 
-$files = scandir($sourcedir);
+$files1 = scandir($sourcedir);
 
 // debugging
-$files=array('2441.1.html');
-$files=array('4592.1.html');
+//$files2=array('2441.1.1.html');
 
 $count = 1;
 
+$files1 = scandir($sourcedir);
+
+foreach ($files1 as $directory)
+{
+	//echo $directory . "\n";
+	if (preg_match('/^\d+$/', $directory))
+	{	
+		//echo $directory . "\n";
+		
+		$files2 = scandir($sourcedir . '/' . $directory);
+
+		foreach ($files2 as $filename)
+		{
+			echo $filename . "\n";
+			if (preg_match('/\.html$/', $filename))
+			{
+			
+				$html = file_get_contents($sourcedir . '/' . $directory . '/' . $filename);
+			
+				$dom = HtmlDomParser::str_get_html($html);
+				
+				//echo $html;
+				
+				$references = array();
+				
+				foreach ($dom->find('div[id=articleCitations] p') as $p)
+				{
+					$text = $p->plaintext;
+					
+					$text = html_entity_decode($text, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+					
+					if (preg_match('/^\s+https?:\/\/(dx.)?doi/', $text))
+					{
+						$n = count($references);
+						$references[$n-1] .= $text;
+					}
+					else
+					{
+						$references[] = $text;
+					}
+					
+				}
+				
+				print_r($references);
+				
+			
+
+			}
+		}
+	}
+}
+
+/*
 
 foreach ($files as $filename)
 {
@@ -79,7 +131,7 @@ foreach ($files as $filename)
 	}
 
 }
-
+*/
 		
 ?>
 
