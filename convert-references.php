02fe452848cb5559bf416de828b41713d1d9be1e
Roderic Page <rdmpage@gmail.com>
2021-07-28 14:27:17 +0100

    Update using code from phytotaxa-cites-data

--- convert-references.php
+++ convert-references.php
@@ -15,8 +15,7 @@ $sourcedir = '/Volumes/Samsung_T5/zootaxa-cites-data/articles';
 $files1 = scandir($sourcedir);
 
 // debugging
-//$files1 = array('2441');
-
+// $files1 = array('2134');
 
 $keys = array('guid', 'author', 'title', 'container-title', 'type', 'volume', 'issue', 'issued', 'page', 'publisher', 'publisher-place', 'editor', 'edition', 'genre', 'note', 'director', 'producer', 'collection-title', 'citation-number', 'translator', 'source', 'URL', 'DOI', 'PMID', 'PMCID', 'isbn', 'medium');
 
@@ -44,6 +43,21 @@ $keys = array(
 	'note',
 	'unstructured'
 	);
+	
+$basic_keys = array(
+	'author', 
+	'issued', 
+	'title', 
+	'container-title', 
+	'volume', 
+	'issue', 
+	'page', 	
+	'publisher', 
+	'publisher-place', 
+	'editor', 
+	'URL', 
+	'note',
+);
 
 // Header row for TSV file
 echo join("\t", $keys) . "\n";
@@ -58,6 +72,8 @@ foreach ($files1 as $directory)
 		//echo $directory . "\n";
 		
 		$files2 = scandir($sourcedir . '/' . $directory);
+		
+		// $files2 = array('2134.1.4.json');
 
 		foreach ($files2 as $filename)
 		{
@@ -99,6 +115,7 @@ foreach ($files1 as $directory)
 				// Get text of references
 				$text_filename = $base_filename . '.txt';
 				$text = file_get_contents($sourcedir . '/' . $directory . '/' . $text_filename);
+				
 
 				// Get CSL JSON and convert to tab delimited row
 				$json = file_get_contents($sourcedir . '/' . $directory . '/' . $filename);
@@ -110,12 +127,21 @@ foreach ($files1 as $directory)
 				if ($references)
 				{
 					// We also want to store unstructured text so that we can reparse if needed
+					
+					// trim start and end
+					$text = preg_replace("/^\s+/", "", $text);
+					$text = preg_replace("/\s+$/", "", $text);
+					
+					
 					$text = preg_replace("/\x{0A}(\x{0A})+/", "\n", $text);
 					$text = preg_replace("/^\x{0A}/", "", $text);
 					$text = preg_replace("/\x{0A}$/", "", $text);
 					
 					$text = preg_replace("/\x{0D}$/", "", $text);
 					
+					// blank lines
+					$text = preg_replace("/\x{0A}(\x{20}\x{0A})+/", "\n", $text);
+										
 					// Text may have blacnk lines, which anystyle will skip, so make sure
 					// we have same number of lines so we can match up strings and refs
 					// echo "\n----\n$text\n----\n";
@@ -141,9 +167,7 @@ foreach ($files1 as $directory)
 								//exit();
 							}
 						}					
-					
-						$values = array();
-						
+
 						$reference->id = $row_count;
 						
 						// So we know what article cites these papers						
@@ -158,6 +182,68 @@ foreach ($files1 as $directory)
 							// so unparsed string so we can check later
 							$reference->unstructured = $rows[$ref_count];
 						}
+						else
+						{
+							// Code here makes a dummy unstructured reference, but also
+							// exits if things fail, so ideally we won't ever use this code.
+							echo "Badness num_rows=$num_rows <> num_refs=$num_refs\n";
+							echo $filename . "\n";
+							exit();
+						
+							// potential ambiguity so "recreate" unstrtuctured reference
+							$values = array();
+							
+							foreach ($basic_keys as $k)
+							{
+								if (isset($reference->{$k}))
+								{
+									if (is_array($reference->{$k}))
+									{
+										$key_value = array();
+										foreach ($reference->{$k} as $v)
+										{
+											$parts = array();
+											if (isset($v->family))
+											{
+												$parts[] = $v->family;
+											}
+											if (isset($v->given))
+											{
+												$parts[] = $v->given;
+											}
+											
+											$key_value[] = join(', ', $parts);
+										}
+										$values[] = join(' ', $key_value);
+									}
+									else
+									{
+										switch ($k)
+										{
+											case 'issued':
+												$values[] ='(' . $reference->{$k} . ')'; 
+												break;
+												
+											default:
+												$values[] = $reference->{$k};
+												break;
+										}
+									
+										
+									}
+								}
+								else
+								{
+									$values[] = "";
+								}
+							}
+							
+							$reference->unstructured = join(' ', $values);
+							$reference->unstructured = preg_replace("/\s\s+/", " ", $reference->unstructured);
+							
+						}
+						
+						$values = array();
 					
 						foreach ($keys as $k)
 						{
